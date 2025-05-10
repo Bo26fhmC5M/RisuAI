0825ad86515e66969cff8180c90564a7ad2b3ae4
Bo26fhmC5M <88071760+Bo26fhmC5M@users.noreply.github.com>
2025-05-10 19:33:04 +0900

    revert: implement lazy loading of chats via lazy portal

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -3,8 +3,8 @@
 export function declareTest() {
     if(import.meta.env.DEV){
         globalThis.test = async () => {
-            const d = await import("./test.js")
-            return d.test()
+            // const d = await import("./test.js")
+            // return d.test()
         }
     }
 }
